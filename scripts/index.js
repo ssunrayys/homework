afae46a788bc39a2bdd3f516a2c0b0e3f635debf
Anna Bondar <Annabondar.pm@gmail.com>
2023-03-09 23:57:27 +0200

    add_js

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,25 @@
 'use strict';
+
+// Task #1
+
+// 1. 2
+// 2. sub 0
+// 3. 1456
+// 4. -69
+// 5. 12
+
+// Task #2
+
+let hours = +prompt('Write hours');
+let seconds = alert(hours * 3600);
+
+// Task #3
+
+let num = 1;
+num += 12;
+num -= 14;
+num *= 5;
+num /= 7;
+num = ++num;
+num = --num;
+alert(num);
