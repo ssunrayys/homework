bb6556c05916b97b8a0058f06c58f10556d01978
Anna Bondar <Annabondar.pm@gmail.com>
2023-03-17 11:17:13 +0200

    first_commit

--- scripts/index.js
+++ scripts/index.js
@@ -3,5 +3,7 @@
 const x = 4;
 const y = 9;
 const r = 10;
+const hypotenuse = x**2 + y**2;
+const z = Math.sqrt(hypotenuse);
 
-console.log((x ))
+z <= r ? console.log('The point is in the circle') : console.log('The point is not in the circle')
