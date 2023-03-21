5a1f24f5f383bc779f4cd3e467e7b132230da945
Anna Bondar <Annabondar.pm@gmail.com>
2023-03-21 20:51:38 +0200

    first_commit

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@
 const base = +prompt("Write a number");
 const exponent = +prompt("Write a power:");
 function power(number, degree = 1) {
-	if (typeof number !== "number" || typeof degree !== "number") {
+	if (Number.isNaN(number)|| Number.isNaN(degree)) {
 		return "Error";
 	}
 	return Math.pow(number, degree);
