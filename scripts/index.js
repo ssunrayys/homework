e7d50b3b1eff219bd6384a9ba5169a930e3a5ff0
Anna Bondar <Annabondar.pm@gmail.com>
2023-03-23 13:39:05 +0200

    first_commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const base = +prompt("Write a number");
-const exponent = +prompt("Write a power:");
-function power(number, degree = 1) {
-	if (Number.isNaN(number)|| Number.isNaN(degree)) {
-		return "Error";
-	}
-	return Math.pow(number, degree);
+const base = prompt("Write a number");
+const exponent = prompt("Write a power:");
+function power(num, degree = 1) {
+	if (num === null || degree === null) return "Error"
+	if (isNaN(num) || isNaN(degree)) return "Error"
+	
+	return Math.pow(num, degree);
 }
 
 const result = power(base, exponent);
