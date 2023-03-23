1a5cb3aa225505c45bf3b42728693790830615fb
Anna Bondar <Annabondar.pm@gmail.com>
2023-03-23 17:13:23 +0200

    first_commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-Homeworks from the course "Frontend Pro" :computer:
+1) using the prompt, we ask the user what he wants to do (add, sub, mult, div).
+
+
+
+2) using prompt we ask the user for the first number.
+
+
+
+3) using Promt we ask the user for the second number.
+
+
+
+4) Using alert or console.log we display the result of the action (add, sub, mult, div) with all operands (eg "2 + 3 = 5" )
--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const question = prompt('What do you want to do? (add, sub, mult, div)');
+const firstNumber = +prompt('Write first number, please');
+const secondNumber = +prompt('Write second number, please');
+
+if (question === 'add') {
+	alert(`The result is ${firstNumber + secondNumber}`);
+}
+else if (question === 'sub') {
+	alert(`The result is ${firstNumber - secondNumber}`);
+}
+else if (question ==='mult') {
+	alert(`The result is ${firstNumber * secondNumber}`);
+}
+else if (question === 'div') {
+	alert(`The result is ${firstNumber / secondNumber}`);
+}
