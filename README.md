c7da7c7f454eec3a2900e5920f8bd2be47145d03
Anna Bondar <Annabondar.pm@gmail.com>
2023-03-23 17:02:49 +0200

    first_commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Homeworks from the course "Frontend Pro" :computer:
+1) we ask the user for 2 operands (2 prompts).
+
+2) write functions for all operations (addition, subtraction, multiplication, division). Each function counts the result and displays in the console "Result: 2+3 = 5", where 2 and 3 are what the user entered, and "+" is the operation for which the function was written
--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const a = +prompt("Write first number, please:");
+const b = +prompt("Write second number, please:");
+
+
+function sum(a, b) {
+	return a + b;
+}
+alert(sum(a, b));
+
+function minus(a, b) {
+	return a - b;
+}
+alert(minus(a, b));
+
+function multiply(a, b) {
+	return a * b;
+}
+alert(multiply(a, b));
+
+function divide(a, b) {
+	return a / b;
+}
+alert(divide(a, b));
