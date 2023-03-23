f9f30574139e031a617bae811a332ec0d828fafd
Anna Bondar <Annabondar.pm@gmail.com>
2023-03-23 17:09:54 +0200

    first_commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// task #1
+const name = "Джон";
+const admin = name;
+alert(admin);
+
+// task #2
+const ourPlanetName = "Earth";
+const currentUserName = "Anna";
+
+// task #3
+
+const value = "10" + 10;
+console.log(typeof (value));
