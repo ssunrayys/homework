da83f92562de808b77dae5143b1434fd31114b60
Anna Bondar <Annabondar.pm@gmail.com>
2023-03-23 15:00:50 +0200

    first_commit

--- scripts/index.js
+++ scripts/index.js
@@ -3,16 +3,17 @@
 const numbers = [1, 2, 3, -1, -2, -3];
 
 function getNewArr(arr) {
-	const emptyArr = [];
+	const emptyArr = []
 	
 	if (arr.length === 0) return 'Array is empty';
 	
 	for (let i = 0; i < arr.length; i++) {
-		if (i >= 0) return emptyArr.push(arr);
+		if (arr[i] >= 0) emptyArr.push(arr[i]);
 	}
 	
 	if (emptyArr.length !== 0) return emptyArr;
 	else return 'Array is empty';
+	
 }
 
 console.log(getNewArr(numbers));
