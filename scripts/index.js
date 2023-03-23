2fabe3fed91f95f50ab2831e7f2889f971123d55
Anna Bondar <Annabondar.pm@gmail.com>
2023-03-23 15:58:05 +0200

    first_commit

--- scripts/index.js
+++ scripts/index.js
@@ -3,9 +3,10 @@
 const waterVolume = 48;
 const weightOfOnePotato = 75;
 const priceForKg = 13;
-function priceOfPotatoes (volume, weight, price) {
-	const weightForDish = Math.ceil(volume * 4 * weight / 1000)
+const sumOfPotatoes = 4;
+function priceOfPotatoes (volume, weight, price, sum) {
+	const weightForDish = Math.ceil(volume * sum * weight / 1000)
 	return weightForDish * price
 }
 
-priceOfPotatoes(waterVolume, weightOfOnePotato, priceForKg);
+priceOfPotatoes(waterVolume, weightOfOnePotato, priceForKg, sumOfPotatoes);
