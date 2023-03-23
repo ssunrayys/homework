a397ac9e882ab4954cddb7d6675bf42418ee9174
Anna Bondar <Annabondar.pm@gmail.com>
2023-03-23 17:39:13 +0200

    first_commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const number = +prompt("Enter a number:");
+
+let isPrime = true;
+
+if (number < 2) {
+	isPrime = false;
+} else {
+	for (let i = 2; i < number; i++) {
+		if (number % i === 0) {
+			isPrime = false;
+			break;
+		}
+	}
+}
+
+console.log(isPrime);
