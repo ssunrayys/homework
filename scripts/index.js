1dd24ec8c17e85bbb4c41160a9e0879896b1056b
Anna Bondar <Annabondar.pm@gmail.com>
2023-04-11 22:59:39 +0300

    third_commit

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,9 @@
 
 // 1.
 
-let arr1 = [1, 2, 3];
-let arr2 = [4, 5, 6];
-let arrConcat = arr1.concat(arr2);
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const arrConcat = arr1.concat(arr2);
 
 // 2.
 
@@ -57,7 +57,48 @@ arr5.push('e');
 
 // 13.
 
+let arr6 = [3, 4, 1, 2, 7];
+arr6.sort();
 
+// 14.
+
+const arr7 = [5, 6, 7, 8, 9];
+const result = arr7.reduce((a, b) => a + b);
+
+
+// 15.
+
+const arr8 =  [1, 2, 3, 4, 5];
+const result3 = arr8.reduce((a, b) => {
+	a.push(b * b);
+	return a;
+}, []);
+
+// 16.
+
+const arr9 = [1,-3, 5, 6,-7, 8, 9,-11];
+const result4 = arr9.filter(a => a < 0);
+
+// 17.
+
+const arr10 = [1,-3, 5, 6,-7, 8, 9,-11];
+const result5 = arr10.filter(a => a % 2 === 0);
+
+// 18.
+
+const arr11 = ['aaa', 'aaaqqq', 'zzzqq', 'zz', 'qsaa', 'q', 'az'];
+const result6 = arr11.filter(a => a.length > 5);
+
+// 19.
+
+const arr12 = [1, 2, [3, 4], 5, [6, 7]];
+const result7 = arr12.filter(Array.isArray);
+
+// 20.
+
+const arr13 = [5,-3, 6,-5, 0,-7, 8, 9];
+const negativeNumbers = arr13.filter(a => a <0);
+const countOfNegativeNumbers = negativeNumbers.length;
 
 
 
