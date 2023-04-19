c7a43fa1f267cdd561725256fe225d0acac6adf4
Anna Bondar <Annabondar.pm@gmail.com>
2023-04-19 11:26:50 +0300

    add code

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-Homeworks from the course "Frontend Pro" :computer:
+1. Create an array arr = ['a', 'b', 'c', 'd'] and use it to display the string 'a+b, c+d'.
+
+2. Create an array arr with elements 2, 5, 3, 9. Multiply the first element of the array by the second and the third element by the fourth. Add the results, assign the variable result. Display the value of this variable
+
+3. Given an array [ [1, 2, 3], [4, 5, 6], [7,8,9] ]. Print the number 4 from this array to the screen.
+
+4. Given an object {js:['jQuery', 'Angular'], php: 'hello', css: 'world'}. Output the word 'jQuery' with it.
+
+5. Fill in the array as follows: write 'x' in the first element, 'xx' in the second, 'xxx' in the third, and so on.
+
+6. Fill the array as follows: write '1' in the first element, '22' in the second, '333' in the third, and so on.
+
+7. Make an arrayFill function that will fill the array with the given values. The first parameter the function takes is the value to fill the array with, and the second parameter is how many elements should be in the array. Example: arrayFill('x', 5) will make an array ['x', 'x', 'x', 'x', 'x'].
+
+8. An array with numbers is given. Find out how many elements from the beginning of the array must be added to make the total more than 10.
+
+9. An array with numbers is given. Without using the reverse method, reverse its elements in reverse order.
+
+10. Given a two-dimensional array with numbers, for example [[1, 2, 3], [4, 5], [6]]. Find the sum of the elements of this array. The array, of course, can be arbitrary.
+
+11. Given a three-dimensional array with numbers, for example [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]. Find the sum of the elements of this array. The array, of course, can be arbitrary.
--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,128 @@
 'use strict';
+
+// 1.
+
+const arr = ['a', 'b','c','d'];
+const str = arr.slice(0, 2).join('+') + ', ' + arr.slice(2, 4).join('+');
+
+alert(str);
+
+// 2.
+
+const arr2 = [2, 5, 3, 9];
+const result = arr2.slice(0, 2).reduce((x, y) => x * y) + arr2.slice(2, 4).reduce((x, y) => x * y);
+console.log(result);
+
+// 3.
+
+const arr3 = [ [1, 2, 3], [4, 5, 6], [7,8,9] ];
+alert(arr3[1][0]);
+
+// 4.
+
+const obj = {js:['jQuery', 'Angular'], php: 'hello', css: 'world'};
+// console.log(obj.js[0]);
+const result2 = obj.js.find(element => element === 'jQuery');
+console.log(result2);
+
+// 5.
+
+const length = 10;
+const arr4 = [];
+
+for (let i = 1; i <= length; i++) {
+	arr4.push('x'.repeat(i));
+}
+
+// 6.
+
+const length2 = 10;
+const arr5 = [];
+
+for (let i = 1; i <= length2; i++) {
+	arr5.push(String(i).repeat(i));
+}
+
+// 7.
+
+const arr6 = [];
+const element = prompt('Write element');
+const count = +prompt('Times of repeat');
+
+function arrayFill(x, y) {
+	for (let i = x; i <= y; i++) {
+		arr6.push(x.repeat(i));
+	}
+}
+
+arrayFill(element, count);
+
+// 8.
+
+const arr7 = [1, 2, 3, 4, 5];
+
+function elementsToAdd(arr) {
+	let sum = 0;
+	let count = 0;
+	
+	arr.some((num, index) => {
+		sum += num;
+		count = index + 1;
+		return sum > 10;
+	});
+	
+	return count;
+}
+
+elementsToAdd(arr7);
+
+// 9.
+
+function reverseArray(arr) {
+	let newArray = [];
+	
+	for (let i = arr.length - 1; i >= 0; i--) {
+		newArray.push(arr[i]);
+	}
+	
+	return newArray;
+}
+
+reverseArray(arr7);
+
+// 10.
+
+const arr8 = [[1, 2, 3], [4, 5], [6]];
+
+function sumOfNestedArray(arr) {
+	const flattenedArray = arr.reduce((accumulator, currentArray) => {
+		return accumulator.concat(currentArray);
+	}, []);
+	
+	const sumOfArr = flattenedArray.reduce((accumulator, currentValue) => {
+		return accumulator + currentValue;
+	}, 0);
+	
+	return sumOfArr;
+}
+
+sumOfNestedArray(arr8);
+
+// 11.
+
+const arr9 = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
+function sumOfArray(arr) {
+	const flattenedArray = arr.reduce((accumulator, currentArray) => {
+		return accumulator.concat(currentArray.reduce((acc, nestedArray) => {
+			return acc.concat(nestedArray);
+		}, []));
+	}, []);
+	
+	const sum = flattenedArray.reduce((accumulator, currentValue) => {
+		return accumulator + currentValue;
+	}, 0);
+	
+	return sum;
+}
+
+sumOfArray(arr9);
