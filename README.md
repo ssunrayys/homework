a4a29ce9c0ead02e2a2da1520c284c3831c19a27
Anna Bondar <Annabondar.pm@gmail.com>
2023-04-19 13:12:28 +0300

    add code

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Homeworks from the course "Frontend Pro" :computer:
+You need to write indexOf , lastIndexOf , find , findIndex , includes , every , and some functions that work just like the array methods of the same name.
+
+Execution algorithm:
+
+Read the documentation for the method you are working on
+understand how it works and what it returns
+write a function that completely mimics how the method works
--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,85 @@
 'use strict';
+
+// indexOf
+
+function indexOf(arr, searchElement, fromIndex = 0) {
+	for (let i = fromIndex; i < arr.length; i++) {
+		if (arr[i] === searchElement) {
+			return i;
+		}
+	}
+	return -1;
+}
+indexOf()
+
+// lastIndexOf
+
+function lastIndexOf(arr, searchElement, fromIndex = arr.length - 1) {
+	for (let i = fromIndex; i >= 0; i--) {
+		if (arr[i] === searchElement) {
+			return i;
+		}
+	}
+	return -1;
+}
+lastIndexOf()
+
+// find
+
+function find(arr, callback) {
+	for (let i = 0; i < arr.length; i++) {
+		if (callback(arr[i], i, arr)) {
+			return arr[i];
+		}
+	}
+	return undefined;
+}
+find()
+
+// findIndex
+
+function findIndex(arr, callback) {
+	for (let i = 0; i < arr.length; i++) {
+		if (callback(arr[i], i, arr)) {
+			return i;
+		}
+	}
+	return -1;
+}
+findIndex()
+
+// includes
+
+function includes(arr, searchElement, fromIndex = 0) {
+	for (let i = fromIndex; i < arr.length; i++) {
+		if (arr[i] === searchElement) {
+			return true;
+		}
+	}
+	return false;
+}
+includes()
+
+// every
+
+function every(arr, callback) {
+	for (let i = 0; i < arr.length; i++) {
+		if (!callback(arr[i], i, arr)) {
+			return false;
+		}
+	}
+	return true;
+}
+every()
+
+// some
+
+function some(arr, callback) {
+	for (let i = 0; i < arr.length; i++) {
+		if (callback(arr[i], i, arr)) {
+			return true;
+		}
+	}
+	return false;
+}
+some()
