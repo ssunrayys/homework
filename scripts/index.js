52d783388eb6de25e4b26744178aac50c67be20f
Anna Bondar <Annabondar.pm@gmail.com>
2023-04-23 18:47:34 +0300

    add code

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,44 @@
 'use strict';
+
+function counter(num = 0) {
+	
+	if (typeof num !== 'number'){
+		throw new Error('Invalid number')
+	}
+	
+	let value = num;
+	let increaseCount = 0;
+	let decreaseCount = 0;
+	let valueCount = 0;
+	
+	return {
+		increase: function () {
+			value++;
+			increaseCount++;
+		},
+		decrease: function () {
+			value--;
+			decreaseCount++;
+		},
+		value: function () {
+			valueCount++;
+			return value;
+		},
+		getStatistic: function () {
+			return {
+				increaseCount,
+				decreaseCount,
+				valueCount
+			};
+		},
+		reset: function () {
+			value = 0;
+			increaseCount = 0;
+			decreaseCount = 0;
+			valueCount = 0;
+		}
+	};
+}
+
+const myCounter = counter(3);
+
