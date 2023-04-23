55ca351e2fd3ef1de43bec06c8afff5fae77461f
Anna Bondar <Annabondar.pm@gmail.com>
2023-04-23 10:52:02 +0300

    add code

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const getRandom = (function () {
+	const usedNums = [];
+	
+	return function () {
+		let randomNum = Math.floor(Math.random() * 100) + 1;
+		if (usedNums.length >= 100) return randomNum;
+		while (usedNums.includes(randomNum)) {
+			randomNum = Math.floor(Math.random() * 100) + 1;
+		}
+		usedNums.push(randomNum);
+		return randomNum;
+	};
+})();
