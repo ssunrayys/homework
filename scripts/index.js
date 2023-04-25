fe88fd9242631d68644c7e145e7301479e79bcac
Anna Bondar <Annabondar.pm@gmail.com>
2023-04-25 23:42:47 +0300

    changes 2

--- scripts/index.js
+++ scripts/index.js
@@ -3,32 +3,96 @@
 // Task 1
 const arr = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
 
-const positiveNumbers = arr.filter(item => item > 0).reduce((acc, item) => acc + item, 0)
+const positiveNumbers = arr.filter(item => item > 0).reduce((acc, item) => acc + item)
 
-let count = 0;
+let countOfPositiveEl = 0;
 
 arr.forEach((item) => {
-	if (item > 0){
-		count++
+	if (item >= 0){
+		countOfPositiveEl++
 	}
 })
 
+
 // Task 2
 
-const minNumber = arr.reduce((item) => Math.min(...arr), [])
+const minNumber = arr.reduce((acc, item) => {
+	if (item < acc) return item
+	else return acc
+})
 
-const numOfMinNumber = arr.indexOf(-63) + 1
+const numOfMinNumber = arr.indexOf(minNumber) + 1
+
 
 // Task 3
 
-const maxNumber = arr.reduce(item => Math.max(...arr), [])
+const maxNumber = arr.reduce((acc, item) => {
+	if (item > acc) return item
+	else return acc
+})
 
-const numOfMaxNumber = arr.indexOf(76) + 1
+const numOfMaxNumber = arr.indexOf(maxNumber) + 1
 
 // Task 4
 
-const negativeNumbers = arr.filter(item => item < 0).length
-
-console.log(negativeNumbers);
+let countOfNegativeEl = 0;
+arr.forEach((item) => {
+	if (item < 0) {
+		countOfNegativeEl++
+	}
+})
 
 // Task 5
+
+const countOfUnpairedNum = arr.reduce((acc, item) => {
+	if(item >= 0 && item % 2 !== 0)
+		return acc + 1
+	else return acc
+}, 0)
+
+
+// Task 6
+
+const countOfDoubleNum = arr.reduce((acc, item) => {
+	if(item >= 0 && item % 2 === 0)
+		return acc + 1
+	else return acc
+}, 0)
+
+// Task 7
+
+const sumOfDoubleEl = arr.reduce((acc, item) => {
+	if(item >= 0 && item % 2 === 0)
+		return acc + item
+	else return acc
+}, 0)
+
+// Task 8
+
+const sumOfUnpairEl = arr.reduce((acc, item) => {
+	if(item >= 0 && item % 2 !== 0)
+		return acc + item
+	else return acc
+}, 0)
+
+//  Task 9
+
+const multOfPositiveEl = arr.reduce((acc, item) => {
+	if(item >= 0)
+		return acc * item
+	else return acc
+})
+
+// Task 10
+
+const maxNumber2 = arr.reduce((acc, item) => {
+	if (item > acc) return item
+	else return acc
+})
+
+const changes = arr.map((item) => {
+	if(item !== maxNumber2) return 0
+	else return item
+})
+
+console.log(changes);
