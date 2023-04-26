106fe925bf03db6a14d899a8ad641e4124975537
Anna Bondar <Annabondar.pm@gmail.com>
2023-04-26 18:29:27 +0300

    changes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Homeworks from the course "Frontend Pro" :computer:
+Create a calculator object with three methods:
+
+read() requests two values and stores them as properties of the object.
+sum() returns the sum of the stored values.
+mul() (multiply) multiplies the stored values and returns the result.
--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,30 @@
 'use strict';
+
+let calculator = {
+	firstNumber: 0,
+	secondNumber: 0,
+	resultOfSum: 0,
+	resultOfMul: 0,
+	
+	read(){
+		this.firstNumber = parseInt(prompt('Write first number'));
+		this.secondNumber = parseInt(prompt('Write second number'));
+	},
+	
+	sum(){
+		
+		if(typeof this.firstNumber === 'number' && typeof this.secondNumber === 'number') {
+			this.resultOfSum = this.firstNumber + this.secondNumber
+		}
+		return this.resultOfSum;
+	},
+	
+	mul(){
+		
+		return this.resultOfMul = this.firstNumber * this.secondNumber;
+	}
+}
+
+calculator.read()
+calculator.sum()
+calculator.mul()
