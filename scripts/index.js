d7d3399b67498112802a882e7229852cccf37d3c
Anna Bondar <Annabondar.pm@gmail.com>
2023-05-08 19:58:49 +0300

    add code

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,19 @@
 'use strict';
+
+let arr = [1, 2, 3];
+function generateList(array){
+	let doc = '<ul>';
+	for (let i = 0; i < array.length; i++) {
+		doc += '<li>';
+		if (Array.isArray(array[i])) {
+			doc += generateList(array[i]);
+		} else {
+			doc += array[i];
+		}
+		doc += '</li>';
+	}
+	doc += '</ul>';
+	return doc;
+}
+
+generateList(arr)
